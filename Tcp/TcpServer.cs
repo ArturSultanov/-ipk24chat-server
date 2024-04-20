da97abcab8f30de816101be2f89f6de7e7658c7e
Artur Sultanov <sultanov.personal@gmail.com>
2024-04-20 19:34:03 +0200

    refactor(): using while (!cancellationToken.IsCancellationRequested) instead of while (!_stopServer) for easy server shutdown

--- Tcp/TcpServer.cs
+++ Tcp/TcpServer.cs
@@ -9,7 +9,6 @@ namespace ipk24chat_server.Tcp;
 
 public class TcpServer()
 {
-    private bool _stopServer = false;
     
     private TcpListener _listener = new TcpListener(ChatSettings.ServerIp, ChatSettings.ServerPort);
 
@@ -18,7 +17,7 @@ public class TcpServer()
         _listener.Start();
         try
         {
-            while (!_stopServer)
+            while (!cancellationToken.IsCancellationRequested)
             {
                 // Wait for a client to connect
                 var tcpClient = await _listener.AcceptTcpClientAsync(cancellationToken);
@@ -35,7 +34,7 @@ public class TcpServer()
                 var user = new TcpUser(tcpClient.Client.RemoteEndPoint, tcpClient);
                 
                 // Add the user to the connected users dictionary
-                ChatUsers.AddUser(user.ConnectionEndPoint, user);
+                ConnectedUsers.AddUser(user.ConnectionEndPoint, user);
                 
                 // Handle the connection in a separate task
                 _ = Task.Run(() => ListenClientAsync(user, cancellationToken), cancellationToken);
@@ -51,71 +50,6 @@ public class TcpServer()
             _listener.Stop();
         }
     }
-    
-    // Listen for messages from the client
-    // private async Task ListenClientAsync(TcpChatUser user, CancellationToken cancellationToken)
-    // {
-    //     try
-    //     {
-    //         using (var stream = user.TcpClient.GetStream())
-    //         {
-    //             byte[] buffer = new byte[4096];
-    //             StringBuilder messageBuilder = new StringBuilder();
-    //             while (!cancellationToken.IsCancellationRequested)
-    //             {
-    //                 int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
-    //                 if (bytesRead > 0)
-    //                 {
-    //                     // Translate data bytes to a ASCII string.
-    //                     string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-    //                     // Append the received data to the StringBuilder.
-    //                     messageBuilder.Append(receivedData);
-    //
-    //                     string messageOneLine = messageBuilder.ToString();
-    //                     // if the message ends with "\r\n" then it is a full message
-    //                     if (messageOneLine.EndsWith("\r\n"))
-    //                     {
-    //                         string[] messages = messageOneLine.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-    //                         foreach (var message in messages)
-    //                         {
-    //                             _messageQueue.Enqueue(MessageToEnvelope(user.ConnectionKey, message));
-    //                             TcpClientMessageTracker.MessageReceived.TrySetResult(true);
-    //                         }
-    //                         messageBuilder.Clear();
-    //                     }
-    //                     else
-    //                     {
-    //                         // When the message is not full, we need to split it by "\r\n" and process all full messages
-    //                         string[] messages = messageOneLine.Split(new[] { "\r\n" }, StringSplitOptions.None);
-    //                         if (messages.Length > 1)
-    //                         {
-    //                             for (int i = 0; i < messages.Length - 1; i++)
-    //                             {
-    //                                 _messageQueue.Enqueue(MessageToEnvelope(user.ConnectionKey, messages[i]));
-    //                                 TcpClientMessageTracker.MessageReceived.TrySetResult(true);
-    //                             }
-    //                             messageBuilder.Clear();
-    //                             messageBuilder.Append(messages[^1]);
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     catch (Exception e)
-    //     {
-    //         Console.WriteLine($"Error in ListenForClientMessagesAsync: {e.Message}");
-    //     }
-    //     finally
-    //     {
-    //         user.TcpClient.Close();
-    //     }
-    //     
-    // }
-    
-    // Check if there is any message in the queue to send to the client
-    // Call the ProcessClientMessagesAsync method to process the messages
-    
     
     private async Task ListenClientAsync(TcpUser user, CancellationToken cancellationToken)
     {
@@ -141,19 +75,8 @@ public class TcpServer()
                 }
             }
         }
-        // catch (IOException e)
-        // {
-        //     Console.WriteLine($"Network error in ListenClientAsync: {e.Message}");
-        // }
-        // catch (Exception e)
-        // {
-        //     Console.WriteLine($"Error in ListenClientAsync: {e.Message}");
-        // }
         finally
         {
-            // user.TcpClient.Close();
-            // // Removing the user from the dictionary
-            // ChatUsers.ConnectedUsers.TryRemove(user.ConnectionEndPoint, out _);
             await user.ClientDisconnect(cancellationToken);
         }
     }
@@ -196,7 +119,7 @@ public class TcpServer()
 
     public void Stop()
     {
-        _stopServer = true;
+        _listener.Stop();
     }
     
 }
